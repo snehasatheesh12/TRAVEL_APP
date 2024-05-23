9d89139a378a1701c3c3288ba0671f71d73c9d11
sneha <snehasatheesh176@gmail.com>
2024-05-23 20:44:20 +0530

    testing-phase

--- travel_app/destinations/tests.py
+++ travel_app/destinations/tests.py
@@ -8,90 +8,53 @@ from rest_framework import status
 from django.contrib.auth.models import User
 
 
-#unit testing
-
-class DestinationModelTest(TestCase):
-    def test_destination_creation(self):
+class DestinationModelTestCase(TestCase):
+    def test_destination_name_max_length(self):
+        max_length_name = Destination._meta.get_field('name').max_length
+        test_name = 'x' * max_length_name
         destination = Destination.objects.create(
+            name=test_name,
+            country='Test Country',
+            description='Test Description',
+            best_time_to_visit='Anytime',
+            category='Beach',
+            image_url='http://example.com/test.jpg'
+        )
+        self.assertEqual(len(destination.name), max_length_name)
+
+
+
+class DestinationModelTestCase(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.destination = Destination.objects.create(
             name='Test Destination',
             country='Test Country',
             description='Test Description',
-            best_time_to_visit='Test Time',
+            best_time_to_visit='Anytime',
             category='Beach',
-            image_url='https://example.com/image.jpg'
+            image_url='http://example.com/test.jpg'
         )
-        self.assertEqual(destination.name, 'Test Destination')
-        self.assertEqual(destination.country, 'Test Country')
-        self.assertEqual(destination.description, 'Test Description')
-        self.assertEqual(destination.best_time_to_visit, 'Test Time')
-        self.assertEqual(destination.category, 'Beach')
-        self.assertEqual(destination.image_url, 'https://example.com/image.jpg')
 
+    def test_destination_name(self):
+        self.assertEqual(self.destination.name, 'Test Destination')
 
+    def test_destination_country(self):
+        self.assertEqual(self.destination.country, 'Test Country')
 
-#integration testing
+    def test_destination_description(self):
+        self.assertEqual(self.destination.description, 'Test Description')
 
-class DestinationAPITest(APITestCase):
-    def setUp(self):
-        self.user = User.objects.create_user(username='testuser', password='testpassword')
-        self.client.force_authenticate(user=self.user)
+    def test_destination_best_time_to_visit(self):
+        self.assertEqual(self.destination.best_time_to_visit, 'Anytime')
 
-    def test_create_destination(self):
-        url = '/api/destinations/'
-        data = {
-            'name': 'Test Destination',
-            'country': 'Test Country',
-            'description': 'Test Description',
-            'best_time_to_visit': 'Test Time',
-            'category': 'Beach',
-            'image_url': 'https://example.com/image.jpg'
-        }
-        response = self.client.post(url, data)
-        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+    def test_destination_category(self):
+        self.assertEqual(self.destination.category, 'Beach')
 
-    def test_get_destination_list(self):
-        url = '/api/destinations/'
-        response = self.client.get(url)
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
+    def test_destination_image_url(self):
+        self.assertEqual(self.destination.image_url, 'http://example.com/test.jpg')
 
-    def test_get_destination_detail(self):
-        destination = Destination.objects.create(
-            name='Test Destination',
-            country='Test Country',
-            description='Test Description',
-            best_time_to_visit='Test Time',
-            category='Beach',
-            image_url='https://example.com/image.jpg'
-        )
-        url = f'/api/destinations/{destination.id}/'
-        response = self.client.get(url)
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
+    def test_str_representation(self):
+        expected_str = 'Test Destination'
+        self.assertEqual(str(self.destination), expected_str)
 
-    def test_update_destination(self):
-        destination = Destination.objects.create(
-            name='Test Destination',
-            country='Test Country',
-            description='Test Description',
-            best_time_to_visit='Test Time',
-            category='Beach',
-            image_url='https://example.com/image.jpg'
-        )
-        url = f'/api/destinations/{destination.id}/'
-        data = {
-            'name': 'Updated Destination Name'
-        }
-        response = self.client.patch(url, data)
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-
-    def test_delete_destination(self):
-        destination = Destination.objects.create(
-            name='Test Destination',
-            country='Test Country',
-            description='Test Description',
-            best_time_to_visit='Test Time',
-            category='Beach',
-            image_url='https://example.com/image.jpg'
-        )
-        url = f'/api/destinations/{destination.id}/'
-        response = self.client.delete(url)
-        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
